4f9faf550cceaac6949e8596b1a8a2a849c53a92
MaximShevchenko12 <maximka.20.05.2008@gmail.com>
2024-02-22 16:52:49 +0300

    added api and xhr for currencies

--- src/main.js
+++ src/main.js
@@ -1,42 +1,20 @@
-// const http = require('http')
-// const fs = require('fs')
-// const mime = require('mime-types')
-
-// let server = http.createServer((req, res) => {
-//     let file_path = "./../public"+req.url    
-//     if (fs.existsSync(file_path)) {
-//         const mime_type = mime.lookup(file_path);
-//         res.writeHead(200, {'Content-Type': mime_type+'; charset=utf-8' })
-//         const stream = fs.createReadStream(file_path)
-//         stream.pipe(res)
-//     } else {
-//         res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8' })   
-//         const stream = fs.createReadStream("./../public/404.html")
-//         stream.pipe(res) 
-//     }
-// })
-
-// const PORT = 3000
-// const HOST = 'localhost'
-
-// server.listen( PORT, HOST, () => {
-//     console.log(`Сервер запущен: http://${HOST}:${PORT}/index.html`)
-// })
-
-
-
-
 const express = require('express')
 const app = express()
 const path = require('path')
 const rates = require('./rates.json');
+const currencies = require('./currencies.json')
 
 app.use('/public', express.static('public'));
 
 app.get('/api/v1/rates', (req, res) => {
     res.json(rates)
     res.sendFile(path.join(__dirname, 'rates.json'))
-    console.log(rates)
+})
+
+app.get('/api/v1/currencies', (req, res) => {
+    res.json(currencies)
+    res.sendFile(path.join(__dirname, 'currencies.json'))
+    console.log(currencies)
 })
 
 app.get('/index.html', (req, res) => {
